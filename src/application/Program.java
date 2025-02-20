b0ea44f209cd7b04c64949f93404465cc907d9ad
Leonardo de Oliveira <toodigital2023@gmail.com>
2025-02-20 20:21:48 -0300

    Implemented findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,13 +13,10 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "book");
-		
-		Seller seller = new Seller(21, "Bob","kl",new Date(), 3000.0,obj);
-		System.out.println(obj);
-		System.out.println(seller);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
 	}
 
 }
