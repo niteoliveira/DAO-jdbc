6e7e5595310bfb05ca0b929b4d78972ed0af2226
Leonardo de Oliveira <toodigital2023@gmail.com>
2025-02-20 15:54:42 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,21 @@
 package application;
 
+
+
+import java.util.Date;
+
 import entities.Department;
+import entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "book");
+		
+		Seller seller = new Seller(21, "Bob","kl",new Date(), 3000.0,obj);
 		System.out.println(obj);
+		System.out.println(seller);
 	}
 
 }
--- src/entities/Seller.java
+++ src/entities/Seller.java
@@ -1,26 +1,32 @@
 package entities;
 
-import java.sql.Date;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
 
-public class Seller {
+public class Seller implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
 	private Integer id;
 	private String email;
 	private String name;
 	private Date birthDate;
 	private Double baseSalary;
 	
+	private Department department;
+	
 	public Seller() {
 		
 	}
 	
-	public Seller(Integer id, String email, String name, Date birthDate, Double baseSalary) {
+	public Seller(Integer id,String name, String email, Date birthDate, Double baseSalary,Department department) {
 		super();
 		this.id = id;
 		this.email = email;
 		this.name = name;
 		this.birthDate = birthDate;
 		this.baseSalary = baseSalary;
+		this.department = department;
 	}
 
 	public Integer getId() {
@@ -64,14 +70,37 @@ public class Seller {
 	}
 	
 	
-	
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return Objects.equals(id, other.id);
+	}
+
 	@Override
 	public String toString() {
 		return "Seller [id=" + id + ", email=" + email + ", name=" + name + ", birthDate=" + birthDate + ", baseSalary="
-				+ baseSalary + "]";
+				+ baseSalary + ", department=" + department + "]";
 	}
-	
-	
+
 	
 	
 }
